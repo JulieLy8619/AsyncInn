a3a203376b2838f3f225e023b498be142ca6099c
juliely8619 <july8619@yahoo.com>
2019-01-27 16:40:55 -0800

    created db tables

---
+++ AsyncInn/Data/HotelMgmtDBContext.cs
@@ -0,0 +1,29 @@
+﻿using AsyncInn.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncInn.Data
+{
+    public class HotelMgmtDBContext : DbContext
+    {
+        public HotelMgmtDBContext(DbContextOptions<HotelMgmtDBContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //adds our composit keys
+        }
+
+        //sets up our tables
+        public DbSet<Amenities> AmenitiesTable {get;set;}
+        public DbSet<Hotel> HotelTable { get; set; }
+        public DbSet<HotelRoom> HotelRoomTable { get; set; }
+        public DbSet<Room> RoomTable { get; set; }
+        public DbSet<RoomAmenities> RoomAmenitiesTable { get; set; }
+    }
+}
