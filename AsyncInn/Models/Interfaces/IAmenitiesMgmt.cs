a297e0fce405d6073890712394e9ea800405c4e9
juliely8619 <july8619@yahoo.com>
2019-01-29 20:10:36 -0800

    working on dependency injection

--- AsyncInn/Models/Interfaces/IAmenitiesMgmt.cs
+++ AsyncInn/Models/Interfaces/IAmenitiesMgmt.cs
@@ -7,5 +7,10 @@ namespace AsyncInn.Models.Interfaces
 {
     interface IAmenitiesMgmt
     {
+        Task CreateAmenity(Amenities amenity);
+        Task<Amenities> GetAmenities(int id);
+        Task<IEnumerable<Amenities>> GetAmenities();
+        void UpdateAmenity(Amenities amenity);
+        void DeleteAmenity(int id);
     }
 }
--- AsyncInn/Models/Services/IAmenitiesMgmtServices.cs
+++ AsyncInn/Models/Services/IAmenitiesMgmtServices.cs
@@ -1,11 +1,44 @@
-﻿using System;
+﻿using AsyncInn.Data;
+using AsyncInn.Models.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace AsyncInn.Models.Services
 {
-    public class IAmenitiesMgmtServices
+    public class IAmenitiesMgmtServices : IAmenitiesMgmt
     {
+        private HotelMgmtDBContext _context { get; }
+        public IAmenitiesMgmtServices(HotelMgmtDBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CreateAmenity(Amenities amenity)
+        {
+            _context.AmenitiesTable.Add(amenity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<Amenities> GetAmenities(int id)
+        {
+            return await _context.AmenitiesTable.FirstOrDefault(amen => amen.ID == id);
+        }
+
+        public async Task<IEnumerable<Amenities>> GetAmenities()
+        {
+            return await _context.AmenitiesTable.
+        }
+
+        public void UpdateAmenity(Amenities amenity)
+        {
+
+        }
+
+        public void DeleteAmenity(int id)
+        {
+            Amenities amenity = _context.AmenitiesTable.FirstOrDefault(amen => amen.ID == id);
+        }
     }
 }
