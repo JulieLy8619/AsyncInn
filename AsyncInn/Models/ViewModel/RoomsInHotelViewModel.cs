b117ff46312340c15956c4ca1e1f5ddfefdca070
juliely8619 <july8619@yahoo.com>
2019-01-31 20:00:28 -0800

    things work again after trying to mess with adding # of rooms at a hotel/# of amenities in a room

---
+++ AsyncInn/Models/ViewModel/RoomsInHotelViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncInn.Models.ViewModel
+{
+    public class RoomsInHotelViewModel
+    {
+        public Hotel Hotel { get; set; }
+        public Room Room { get; set; }
+    }
+}
